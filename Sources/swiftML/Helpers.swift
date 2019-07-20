85f301c5bf0c9d734453027fe77756c318d4d12e
param087 <bhavsarparam087@gmail.com>
2019-07-20 11:52:31 +0530

    implemented linear regression

---
+++ Sources/swiftML/Helpers.swift
@@ -0,0 +1,80 @@
+import TensorFlow
+
+/// The generalized inverse of a matrix using its singular-value decomposition (SVD).
+///
+/// Return the (Moore-Penrose) pseudo-inverse of a matrix.
+///
+/// - Parameter x: The matrix to pseudo-inverse.
+/// - Returns: Pseudo-inverse matrix.
+public func matrixPseudoInverse(_ data: Tensor<Float>) -> Tensor<Float> {
+    let svd = Raw.svd(data)
+    let diag = Raw.diag(diagonal: svd.s)
+    return matmul(matmul(svd.v, Raw.matrixInverse(diag)), svd.u.transposed())
+}
+
+/// Return Sign correction to ensure deterministic output from SVD.
+///
+/// - Parameters
+///   - u: The tensor containing of left singular vectors for each matrix.
+///   - v: The tensor containing of right singular vectors for each matrix.
+/// - Returns: Sign correction to ensure deterministic output.
+public func svdFlip(
+    u: Tensor<Float>,
+    v: Tensor<Float>,
+    uBasedDecision: Bool = true
+) -> (Tensor<Float>, Tensor<Float>) {
+  
+    var U = u
+    var V = v
+    
+    if uBasedDecision {
+    
+        let maxAbsCols = abs(u).argmax(squeezingAxis: 0)
+        var colValueForSign = Tensor<Float>(zeros: [u.shape[1]])
+        
+        for i in 0..<u.shape[1] {
+            colValueForSign[i] = u[Int(maxAbsCols[i].scalarized()), i]
+        }
+        
+        let signs = Raw.sign(colValueForSign)
+        U = U * signs
+        V = V * signs.reshaped(to: [u.shape[1], 1])
+        
+    } else {
+        
+        let maxAbsRows = abs(v).argmax(squeezingAxis: 1)
+        var rowValueForSign = Tensor<Float>(zeros: [u.shape[0]])
+        
+        for i in 0..<u.shape[0] {
+            rowValueForSign[i] = v[i, Int(maxAbsRows[i].scalarized())]
+        }
+        
+        let signs = Raw.sign(rowValueForSign)
+        U = U * signs
+        V = V * signs.reshaped(to: [u.shape[1], 1])
+    }
+    
+    return (U, V)
+}
+
+  
+/// Return the minkowski distance based on value of p.
+///
+/// - Parameters
+///   - a: Input tensor to find distance.
+///   - b: Input tensor to find distance.
+///   - p: The distance metric to use for the tree, default set to 2.
+/// - Returns: Minkowski distance based on value of p.
+public func minkowskiDistance(_ a: Tensor<Float>, _ b: Tensor<Float>, _ p: Int ) -> Tensor<Float> {
+    return pow(pow(abs(b - a), Float(p)).sum(), 1.0/Float(p))
+}
+
+/// Return the euclidean distance.
+///
+/// - Parameters
+///   - a: Input tensor to find distance.
+///   - b: Input tensor to find distance.
+/// - Returns: euclidean distance.
+public func euclideanDistance(_ a: Tensor<Float>, _ b: Tensor<Float>) -> Tensor<Float> {
+    return minkowskiDistance(a, b, 2)
+}
---
+++ Sources/swiftML/LinearRegression.swift
@@ -0,0 +1,130 @@
+import TensorFlow
+
+/// A linear regression model
+///
+/// Linear regression with support for gradient descent and singular vector decomposition (svd).
+public class LinearRegression {
+
+    var iterations: Int
+    var learningRate: Float
+    var gradientDescent: Bool
+    var fitIntercept: Bool
+    public var weights: Tensor<Float>
+
+    /// Creates a linear regression model.
+    ///
+    /// - Parameters
+    ///   - gradientDescent: The gradient descent or singular vector decomposition method to be
+    ///     used for learning, default to 'false'.
+    ///   - iterations: The number of iterations for gradient descent, default to 1000.
+    ///   - learningRate: The learning rate for gardient descent, default to 0.001
+    ///   - fitIntercept: whether to calculate the intercept for this model. If set to False, no
+    ///     intercept will be used in calculations, default set to true.
+    public init(
+        gradientDescent: Bool = false,
+        iterations: Int = 1000,
+        learningRate: Float = 0.001,
+        fitIntercept: Bool = true
+    ) {
+        precondition(iterations > 0, "Number of iterations must be greater than zero.")
+        precondition(learningRate >= 0, "Learning Rate must be non-negative.")
+
+        self.iterations = iterations
+        self.learningRate = learningRate
+        self.gradientDescent = gradientDescent
+        self.weights = Tensor<Float>(0)
+        self.fitIntercept = fitIntercept
+    }
+
+    /// Initialize weights between [-1/N, 1/N].
+    /// - Parameters featuresCount: The number of features in training.
+    internal func initializeWeights(featuresCount: Int) {
+    
+        var w = Tensor<Float>(zeros: [featuresCount, 1])
+        let limit: Float = 1 / sqrt(Float(featuresCount))
+    
+        for i in 0..<featuresCount {
+            w[i] = Tensor<Float>([Float.random(in: -limit...limit)])
+        }
+        // assign weights with randomly initialize weights.
+        self.weights = w
+    }
+    
+    /// Fit linear model.
+    ///
+    /// - Parameters
+    ///   - data: Training data Tensor<Float> of shape [number of samples, number of features].
+    ///   - labels: Target value Tensor<Float> of shape [number of samples, 1].
+    public func fit(data: Tensor<Float>, labels: Tensor<Float>) {
+        
+        precondition(data.shape[0] == labels.shape[0],
+            "data and labels must have same number of samples.")
+        precondition(data.shape[0] > 0, "data must be non-empty.")
+        precondition(data.shape[1] >= 1, "data must have atleast single feature.")
+        precondition(data.shape[0] > 0, "labels must be non-empty.")
+        precondition(labels.shape[1] == 1, "labels must have single target.")
+
+        var modifiedData: Tensor<Float> = data
+       
+        if self.fitIntercept {
+            let ones = Tensor<Float>(ones: [data.shape[0], 1])
+            modifiedData = ones.concatenated(with: data, alongAxis: -1)
+        }
+
+        if self.gradientDescent == false {       
+            let svd = Raw.svd(matmul(modifiedData.transposed(), modifiedData))
+            let s = Raw.diag(diagonal: svd.s)
+            let dataSqRegInv = matmul(matmul(svd.v, matrixPseudoInverse(s)), svd.u.transposed())
+            self.weights = matmul(matmul(dataSqRegInv, modifiedData.transposed()), labels)
+        } else {
+            self.initializeWeights(featuresCount: modifiedData.shape[1])
+
+            for _ in 0..<self.iterations {
+                let predictedLabels = matmul(modifiedData, self.weights)
+                let weightsGradient =
+                    -1 * matmul(modifiedData.transposed(), (labels - predictedLabels))
+                self.weights = self.weights - (self.learningRate * weightsGradient)
+            }
+        }
+    }
+
+    /// Returns predict using linear model.
+    ///
+    /// - Parameter data: Sample data tensor of shape [number of samples, number of features].
+    /// - Returns: Predicted value tensor.
+    public func prediction(for data: Tensor<Float>) -> Tensor<Float> {
+    
+        precondition(data.shape[0] > 0, "data must be non-empty.")
+
+        var modifiedData: Tensor<Float> = data
+       
+        if self.fitIntercept {
+            let ones = Tensor<Float>(ones: [data.shape[0], 1])
+            modifiedData = ones.concatenated(with: data, alongAxis: -1)
+        }
+
+        let predictedLabels = matmul(modifiedData, self.weights)
+        return predictedLabels
+    }
+
+    /// Returns the coefficient of determination R^2 of the prediction.
+    ///
+    /// - Parameters
+    ///   - data: Sample tensor of shape [number of samples, number of features].
+    ///   - labels: Target value tensor of shape [number of samples, 1].
+    /// - Returns: The coefficient of determination R^2 of the prediction.
+    public func score(data: Tensor<Float>, labels: Tensor<Float>) -> Float {
+
+        precondition(data.shape[0] == labels.shape[0],
+            "data and labels must have same number of samples.")
+        precondition(data.shape[0] > 0, "data must be non-empty.")
+        precondition(labels.shape[0] > 0, "labels must be non-empty.")
+        
+        let predictedLabels = self.prediction(for: data)
+        let u = pow((labels - predictedLabels),2).sum()
+        let v = pow((labels - labels.mean()), 2).sum()
+        let score = (1 - (u/v))
+        
+        return Float(score.scalarized())
+    }
+}
