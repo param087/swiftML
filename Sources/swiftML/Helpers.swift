860a727704f1b3e888a6457c7f87848613614018
param087 <bhavsarparam087@gmail.com>
2019-08-07 08:09:51 +0530

    Remove svdFlip and replace with deterministicSvd - added reference link

--- Sources/swiftML/Helpers.swift
+++ Sources/swiftML/Helpers.swift
@@ -2,6 +2,9 @@ import TensorFlow
 
 extension Tensor where Scalar: TensorFlowFloatingPoint {
     /// The (Moore-Penrose) pseudoinverse of a matrix using its singular-value decomposition (SVD).
+    ///
+    /// Reference: ["Moore–Penrose inverse"](
+    /// https://en.wikipedia.org/wiki/Moore%E2%80%93Penrose_inverse)
     public var pseudoinverse: Tensor {
         let svd = Raw.svd(self)
         let diag = Raw.diag(diagonal: svd.s)
@@ -9,39 +12,48 @@ extension Tensor where Scalar: TensorFlowFloatingPoint {
     }
 }
 
-/// Returns sign-corrected versions of the left and right singular vectors from
-/// single value decomposition (SVD).
+/// Returns deterministic SVD contains sign-corrected versions of left singular vectors and right
+/// singular vectors from input matrix.
+///
+/// Reference: ["Determinitic SVD"](https://github.com/scikit-learn/
+/// scikit-learn/blob/53f76d1a24ef42eb8c620fc1116c53db11dd07d9/sklearn/utils/extmath.py#L482)
 ///
 /// - Parameters:
-///   - u: The tensor containing the left singular vectors for each matrix.
-///   - v: The tensor containing the right singular vectors for each matrix.
-/// - Returns: The sign corrected versions of `u` and `v` to ensure
-///   deterministic output.
-public func svdFlip<Scalar: TensorFlowFloatingPoint>(
-    u: Tensor<Scalar>,
-    v: Tensor<Scalar>,
-    uBasedDecision: Bool = true
-) -> (Tensor<Scalar>, Tensor<Scalar>) {
-    let signs: Tensor<Scalar>
-    if uBasedDecision {
+///   - input: The input matrix.
+///   - columnBasedSignFlipping - If `true`, use the columns of `u` as the basis for sign flipping.
+///     Otherwise, use the rows of v. The choice of which variable to base the decision on is
+///     generally algorithm dependent, default to `true`.
+/// - Returns: The sign corrected svd to ensure deterministic output.
+public func deterministicSvd<T: FloatingPoint & TensorFlowScalar>(
+    _ input: Tensor<T>,
+    columnBasedSignFlipping: Bool = true
+) -> (s: Tensor<T>, u: Tensor<T>, v: Tensor<T>) {
+    var (s, u, v) = Raw.svd(input)
+    v = v.transposed()
+
+    let signs: Tensor<T>
+    
+    if columnBasedSignFlipping {
         let maxAbsCols = abs(u).argmax(squeezingAxis: 0)
-        var colValueForSign = Tensor<Scalar>(zeros: [u.shape[1]])
+        var colValueForSign = Tensor<T>(zeros: [u.shape[1]])
         for i in 0..<u.shape[1] {
             colValueForSign[i] = u[Int(maxAbsCols[i].scalarized()), i]
         }
         signs = Raw.sign(colValueForSign)
     } else {
         let maxAbsRows = abs(v).argmax(squeezingAxis: 1)
-        var rowValueForSign = Tensor<Scalar>(zeros: [u.shape[0]])
+        var rowValueForSign = Tensor<T>(zeros: [u.shape[0]])
         for i in 0..<u.shape[0] {
             rowValueForSign[i] = v[i, Int(maxAbsRows[i].scalarized())]
         }
         signs = Raw.sign(rowValueForSign)
     }
-    return (u * signs, v * signs.reshaped(to: [u.shape[1], 1]))
+    return (s, u * signs, v * signs.reshaped(to: [u.shape[1], 1]))
 }
 
 /// Returns the Minkowski distance between two tensors for the given distance metric `p`.
+///
+/// Reference: ["Minkowski distance"](https://en.wikipedia.org/wiki/Minkowski_distance)
 ///
 /// - Parameters:
 ///   - a: The first tensor.
@@ -57,6 +69,8 @@ public func minkowskiDistance<Scalar: TensorFlowFloatingPoint>(
 }
 
 /// Returns the Euclidean distance between two tensors.
+///
+/// Reference: ["Euclidean distance"](https://en.wikipedia.org/wiki/Euclidean_distance)
 ///
 /// - Parameters:
 ///   - a: The first tensor.
