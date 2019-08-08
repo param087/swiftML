39703e92b75a666304d05d7e8a09f40e3a205031
param087 <bhavsarparam087@gmail.com>
2019-08-09 00:01:43 +0530

    Polish logisticRegression.swift - Changed data type of `labels` form `Tensor<Float>` to `Tensor<Int32>` - Added doc comments to stored properties - Updated logistic regression notebook

--- Sources/swiftML/Helpers.swift
+++ Sources/swiftML/Helpers.swift
@@ -1,90 +1,5 @@
 import TensorFlow
 
-<<<<<<< HEAD
-/// The generalized inverse of a matrix using its singular-value decomposition (SVD).
-///
-/// Returns the (Moore-Penrose) pseudo-inverse of a matrix.
-///
-/// - Parameter data: The input matrix to be pseudo-inverse.
-/// - Returns: Pseudo-inverse matrix.
-public func matrixPseudoInverse(_ data: Tensor<Float>) -> Tensor<Float> {
-    let svd = Raw.svd(data)
-    let diag = Raw.diag(diagonal: svd.s)
-    return matmul(matmul(svd.v, Raw.matrixInverse(diag)), svd.u.transposed())
-}
-
-/// Returns Sign correction to ensure deterministic output from SVD.
-///
-/// - Parameters
-///   - u: The tensor containing of left singular vectors for each matrix.
-///   - v: The tensor containing of right singular vectors for each matrix.
-/// - Returns: Sign correction to ensure deterministic output.
-public func svdFlip(
-    u: Tensor<Double>,
-    v: Tensor<Double>,
-    uBasedDecision: Bool = true
-) -> (Tensor<Double>, Tensor<Double>) {
-  
-    var u = u
-    var v = v
-    
-    if uBasedDecision {
-    
-        let maxAbsCols = abs(u).argmax(squeezingAxis: 0)
-        var colValueForSign = Tensor<Double>(zeros: [u.shape[1]])
-        
-        for i in 0..<u.shape[1] {
-            colValueForSign[i] = u[Int(maxAbsCols[i].scalarized()), i]
-        }
-        
-        let signs = Raw.sign(colValueForSign)
-        u = u * signs
-        v = v * signs.reshaped(to: [u.shape[1], 1])
-        
-    } else {
-        
-        let maxAbsRows = abs(v).argmax(squeezingAxis: 1)
-        var rowValueForSign = Tensor<Double>(zeros: [u.shape[0]])
-        
-        for i in 0..<u.shape[0] {
-            rowValueForSign[i] = v[i, Int(maxAbsRows[i].scalarized())]
-        }
-        
-        let signs = Raw.sign(rowValueForSign)
-        u = u * signs
-        v = v * signs.reshaped(to: [u.shape[1], 1])
-    }
-    
-    return (u, v)
-}
-
-  
-/// Returns the Minkowski distance between two tensors for the given distance metric `p`.
-///
-/// - Parameters
-///   - a: The first tensor.
-///   - b: The second tensor.
-///   - p: The order of the norm of the difference: `||a - b||_p`.
-/// - Returns: Minkowski distance based on value of `p`.
-public func minkowskiDistance(_ a: Tensor<Float>, _ b: Tensor<Float>, p: Int ) -> Tensor<Float> {
-    
-    precondition(a.shape == b.shape,"Shape of both the inputs must be same.")
-    precondition(p > 0, "p must be greater than zero.")
-
-    return pow(pow(abs(b - a), Float(p)).sum(), 1.0/Float(p))
-}
-
-/// Returns the euclidean distance between two tensors.
-///
-/// - Parameters
-///   - a: The first tensor.
-///   - b: The second tensor.
-/// - Returns: Euclidean distance: `||a - b||_2`.
-public func euclideanDistance(_ a: Tensor<Float>, _ b: Tensor<Float>) -> Tensor<Float> {
-
-    precondition(a.shape == b.shape,"Shape of both the inputs must be same.")
-    
-=======
 extension Tensor where Scalar: TensorFlowFloatingPoint {
     /// The (Moore-Penrose) pseudoinverse of a matrix using its singular-value decomposition (SVD).
     ///
@@ -165,6 +80,5 @@ public func euclideanDistance<Scalar: TensorFlowFloatingPoint>(
     _ a: Tensor<Scalar>, _ b: Tensor<Scalar>, p: Int
 ) -> Tensor<Scalar> {
     precondition(a.shape == b.shape, "Both inputs must have the same shape.")
->>>>>>> 068e99a74c4c913f173f9445a95440eab4ee503f
     return minkowskiDistance(a, b, p: 2)
 }
--- Sources/swiftML/LogisticRegression.swift
+++ Sources/swiftML/LogisticRegression.swift
@@ -2,54 +2,55 @@ import TensorFlow
 
 /// A Logistic regression classifier.
 ///
-/// In the multiclass case, the training algorithm uses the one-vs-rest (OvR) scheme.
+/// In the multiclass classification, the training algorithm uses the one-vs-rest (OvR)
+/// scheme.
 public class LogisticRegression {
-
+    ///  The linear rate for gradient descent.
     var learningRate: Float
-    var iterations: Int
+    /// The number of iterations for gradient descent.
+    var iterationCount: Int
+    /// Whether to calculate the intercept for this model.
     var fitIntercept: Bool
-    // The tensor of unique classes
+    /// The tensor of unique classes.
     var classes: Tensor<Int32>
-    // The tensor contains the index to unique classes
+    /// The tensor contains the index to unique classes.
     var indices: Tensor<Int32>
+    /// The weights array of the model contains weights of each class agains the rest.
     public var weights = [Tensor<Float>]()
     
     /// Creates a logistic regression model.
     ///
     /// - Parameters
-    ///   - iterations: The number of iterations for gradient descent, default to `0.1`.
+    ///   - iterationCount: The number of iterations for gradient descent, default to `0.1`.
     ///   - learningRate: The learning rate for gardient descent, default to `1000`.
     ///   - fitIntercept: Whether to calculate the intercept for this model. If set to `false`, no
     ///     intercept will be used in calculations, default set to `true`.
     public init(
         learningRate: Float = 0.1,
-        iterations: Int = 1000,
+        iterationCount: Int = 1000,
         fitIntercept: Bool = true
     ) {
-
-        precondition(iterations > 0, "Number of iterations must be greater than zero.")
-        precondition(learningRate >= 0, "Learning Rate must be non-negative.")
-
+        precondition(iterationCount > 0, "Iteration count must be positive.")
+        precondition(learningRate >= 0, "Learning rate must be non-negative.")
         self.learningRate = learningRate
-        self.iterations = iterations
+        self.iterationCount = iterationCount
         self.fitIntercept = fitIntercept
         self.classes = Tensor<Int32>(0)
         self.indices = Tensor<Int32>(0)
     }
     
-    /// Fit logistic regression model.
+    /// Fit a logistic regression model.
     ///
     /// - Parameters
-    ///   - data: Training data tensor of shape [number of samples, number of features].
-    ///   - labels: Target value tensor of shape [number of samples, 1].
-    public func fit(data: Tensor<Float>, labels: Tensor<Float>) {
-
+    ///   - data: Training data with shape `[sample count, feature count]`.
+    ///   - labels: Target value with shape `[sample count, 1]`.
+    public func fit(data: Tensor<Float>, labels: Tensor<Int32>) {
+        precondition(data.shape[0] > 0, "Data must have a positive sample count.")
+        precondition(data.shape[1] >= 1, "Data must have feature count greater than one.")
+        precondition(labels.shape[0] > 0, "Labels must have a positive sample count.")
+        precondition(labels.shape[1] == 1, "Labels must have single target feature.")
         precondition(data.shape[0] == labels.shape[0],
-            "data and labels must have same number of samples.")
-        precondition(data.shape[0] > 0, "data must be non-empty.")
-        precondition(data.shape[1] >= 1, "data must have atleast single feature.")
-        precondition(labels.shape[0] > 0, "labels must be non-empty.")
-        precondition(labels.shape[1] == 1, "labels must have single feature target.")
+            "Data and labels must have the same sample count.")
 
         var modifiedData = data
 
@@ -59,27 +60,25 @@ public class LogisticRegression {
         }
         let numberOfSamples = Float(modifiedData.shape[0])
 
-        let labels = Tensor<Int32>(labels)
         (self.classes, self.indices) = Raw.unique(labels.flattened())
 
-        precondition(self.classes.shape[0] >= 2, "labels must contains atleast 2 classes")
+        precondition(self.classes.shape[0] >= 2, "Labels must have atleast two classes")
         
-        // loop through each class, uses the one-vs-rest (OvR) scheme. 
+        /// Loop through each class and apply one-vs-rest (OvR) scheme. 
         for i in 0..<self.classes.shape[0] {
-
             let condition = Raw.equal(labels, classes[i])
             let t = Tensor<Int32>(ones: [labels.shape[0], 1])
             let e = Tensor<Int32>(zeros: [labels.shape[0], 1])
 
-            // create temparary label for one-vs-rest scheme, based on class the selected class
-            // labeled as one while rest as zeros.
+            /// Create temparary labels for one-vs-rest scheme, based on class the selected class
+            /// labeled as `1` while rest as `0`.
             var tempLabels = Tensor<Float>(Raw.select(condition: condition, t: t, e: e))
             tempLabels = tempLabels.reshaped(to: [tempLabels.shape[0], 1])
     
-            // weights of selected class in one-vs-rests scheme.
+            /// weights of selected class in one-vs-rests scheme.
             var tempWeights = Tensor<Float>(ones: [modifiedData.shape[1], 1])
 
-            for _ in 0..<self.iterations {
+            for _ in 0..<self.iterationCount {
                 let output = matmul(modifiedData, tempWeights)
                 let errors = tempLabels - sigmoid(output)
                 tempWeights = tempWeights +
@@ -91,11 +90,11 @@ public class LogisticRegression {
         }
     }
     
-    /// Return the prediction of single sample
+    /// Return the prediction for a single sample.
     ///
-    /// - Parameters data: Sample tuple tensor of shape [1, number of features].
+    /// - Parameters data: Single sample data with shape `[1, feature count]`.
     /// - Returns: Predicted class label.
-    public func predictSingleSample(_ data: Tensor<Float>) -> Tensor<Float> {
+    public func predictSingleSample(_ data: Tensor<Float>) -> Tensor<Int32> {
         var output = Tensor<Float>(zeros: [weights.count, 1])
         var counter: Int = 0
 
@@ -108,15 +107,14 @@ public class LogisticRegression {
         let index: Int = Int(output.argmax().scalarized())
         let classLabel: Tensor<Int32> = self.classes[index]
 
-        return Tensor<Float>([Tensor<Float>(classLabel)])
+        return Tensor<Int32>([classLabel])
     }
     
-    /// Returns predict using logistic regression classifier.
+    /// Returns prediction using logistic regression classifier.
     ///
-    /// - Parameter data: Smaple data tensor of shape [number of samples, number of features].
-    /// - Returns: Predicted target tensor of class labels.
-    public func prediction(for data: Tensor<Float>) -> Tensor<Float> {
-
+    /// - Parameter data: Smaple data with shape `[sample count, feature count]`.
+    /// - Returns: Predicted class label of target values.
+    public func prediction(for data: Tensor<Float>) -> Tensor<Int32> {
         precondition(data.shape[0] > 0, "data must be non-empty.")
 
         var modifiedData = data
@@ -125,7 +123,7 @@ public class LogisticRegression {
             modifiedData = ones.concatenated(with: data, alongAxis: -1)
         }
         
-        var result = Tensor<Float>(zeros: [modifiedData.shape[0], 1])
+        var result = Tensor<Int32>(zeros: [modifiedData.shape[0], 1])
         for i in 0..<modifiedData.shape[0] {
             result[i] = self.predictSingleSample(modifiedData[i])
         }
@@ -135,15 +133,14 @@ public class LogisticRegression {
     /// Returns mean accuracy on the given test data and labels.
     ///
     /// - Parameters
-    ///   - data: Sample tensor of shape [number of samples, number of features].
-    ///   - labels: Target label tensor of shape [number of samples, 1].
-    /// - Returns: Returns the mean accuracy on the given test data and labels.
-    public func score(data: Tensor<Float>, labels: Tensor<Float>) -> Float {
-
+    ///   - data: Sample data with shape `[sample count, feature count].
+    ///   - labels: Target label with shape `[sample count, 1]`.
+    /// - Returns: Returns the mean accuracy on the given test data 1and labels.
+    public func score(data: Tensor<Float>, labels: Tensor<Int32>) -> Float {
+        precondition(data.shape[0] > 0, "Data must have a positive sample count.")
+        precondition(labels.shape[0] > 0, "Labels must have a positive sample count.")
         precondition(data.shape[0] == labels.shape[0],
-            "data and labels must have same number of samples.")
-        precondition(data.shape[0] > 0, "data must be non-empty.")
-        precondition(labels.shape[0] > 0, "labels must be non-empty.")
+            "Data and labels must have the same sample count.")
 
         let result = self.prediction(for: data)
         var count: Int = 0
