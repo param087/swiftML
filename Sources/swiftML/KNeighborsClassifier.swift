cd9b6611d13815f80c1f654f179e6f1ea1dce376
param087 <bhavsarparam087@gmail.com>
2019-08-11 14:15:20 +0530

    Polished `KNeighborsClassifier.swift` and `KNeighborsRegressor.swift` - Updated notebooks - Added doc comment to stored properties - Change data type of `labels` in `KNeighborsClassifier.swift` from `Tenosr<Float>` to `Tensor<Int32>` - Change `neighbors` to `neighborCount` in both `KNeighborsClassifier.swift` and `KNeighborsRegressor.swift`.

--- Sources/swiftML/KNeighborsClassifier.swift
+++ Sources/swiftML/KNeighborsClassifier.swift
@@ -4,60 +4,63 @@ import TensorFlow
 ///
 /// Classifier implementing the k-nearest neighbors vote.
 public class KNeighborsClassifier {
-
+    /// The order of the norm of the difference: `||a - b||_p`.
     var p: Int
+    /// Weight function used in prediction.
     var weights: String
+    /// Number of neighbors.
+    var neighborCount: Int
+    /// The training data.
     var data: Tensor<Float>
-    var labels: Tensor<Float>
-    var neighbors: Int
+    /// The target class correspoing to training data.
+    var labels: Tensor<Int32>
   
     /// Create a K neighbors classifier model.
     ///
     /// - Parameters
-    ///   - neighbors: Number of neighbors to use, default to `5`.
+    ///   - neighborCount: Number of neighbors to use, default to `5`.
     ///   - weights: Weight function used in prediction. Possible values `uniform` - uniform
-    ///     weighted, `distance` - weight point by inverse of thier distance. default set to
+    ///     weighted, `distance` - weight point by inverse of their distance. default set to
     ///     `distance`.
     ///   - p: The order of the norm of the difference: `||a - b||_p`, default set to `2`.
     public init(
-        neighbors: Int = 5,
+        neighborCount: Int = 5,
         weights: String = "distance",
         p: Int = 2
     ) {
-        precondition(neighbors > 1, "Neighbors must be greater than one.")
+        precondition(neighborCount > 1, "Neighbor count must be greater than one.")
         precondition(weights == "uniform" || weights == "distance",
-            "weights must be either 'uniform' or 'distance'.")
-        precondition(p > 1, "p must be greater than one.")
+            "Weights must be either 'uniform' or 'distance'.")
+        precondition(p > 1, "p must be positive.")
 
-        self.neighbors = neighbors
+        self.neighborCount = neighborCount
         self.weights = weights
         self.p = p
         self.data = Tensor<Float>([0.0])
-        self.labels = Tensor<Float>([0.0])
+        self.labels = Tensor<Int32>([0])
     }
   
-    /// fit Kneighbors classifier model.
+    /// Fit a K-neighbors classifier model.
     ///
     /// - Parameters
-    ///   - data: Training data tensor of shape [number of samples, number of features].
-    ///   - labels: Target value tensor of shape [number of samples].
-    public func fit(data: Tensor<Float>, labels: Tensor<Float>) {
-
+    ///   - data: Training data with shape `[sample count, feature count]`.
+    ///   - labels: Target value with shape `[sample count]`.
+    public func fit(data: Tensor<Float>, labels: Tensor<Int32>) {
         precondition(data.shape[0] == labels.shape[0],
-            "data and labels must have same number of samples.")
-        precondition(data.shape[0] > 0, "data must be non-empty.")
-        precondition(data.shape[1] >= 1, "data must have atleast single feature.")
-        precondition(labels.shape[0] > 0, "labels must be non-empty.")
+            "Data and labels must have same number of samples.")
+        precondition(data.shape[0] > 0, "Data must be non-empty.")
+        precondition(data.shape[1] >= 1, "Data must have atleast single feature.")
+        precondition(labels.shape[0] > 0, "Labels must be non-empty.")
 
         self.data = data
         self.labels = labels
     }
 
-    /// Return the weights of each neighbors.
+    /// Returns the weights of each neighbor.
     ///
     /// - Parameters
-    ///   - distances: Tensor contains the distance between test tensor and top neighbors.
-    ///   - labels: Tensor contains the classes of neighbors.
+    ///   - distances: Contains the distance between test data and top neighbors.
+    ///   - labels: Contains the classes of neighbors.
     /// - Returns - The weights of each neighbors.
     internal func computeWeights(
         distances: Tensor<Float>,
@@ -66,8 +69,8 @@ public class KNeighborsClassifier {
   
         var labelsAndWeightsTensor = Tensor<Float>(zeros:[distances.shape[0], 2])
         
-        // In uniform weighing method each class have same weight.
-        // In distance weighing method weight vary based on the inverse of distance.
+        // In `uniform` weighing method each class have same weight.
+        // In `distance` weighing method weight vary based on the inverse of distance.
         if self.weights == "uniform" {
             for i in 0..<distances.shape[0] {
                 labelsAndWeightsTensor[i][0] = labels[i]
@@ -86,77 +89,77 @@ public class KNeighborsClassifier {
                 labelsAndWeightsTensor[i][1] = Tensor<Float>(1.0 / distances[i])
             }
             return labelsAndWeightsTensor
-        }  
+        }
         return Tensor<Float>(0.0)
     } 
 
-    /// Returns the individual tensor predicted class.
+    /// Returns the predicted classification.
     ///
-    /// - Parameter test: Input tensor to be classified.
-    /// - Returns: Predicted class.
-    internal func predictSingleSample(_ test: Tensor<Float>) -> Tensor<Float> {
-
+    /// - Parameter test: Input data to be classified.
+    /// - Returns: Predicted classification.
+    internal func predictSingleSample(_ test: Tensor<Float>) -> Tensor<Int32> {
         var distances = Tensor<Float>(zeros: [self.data.shape[0]])
-        var maxLabel = Tensor<Float>(zeros: [self.neighbors])
-        
+        var maxLabel = Tensor<Int32>(zeros: [self.neighborCount])
         var maxDistances: Tensor<Float>
         var maxIndex: Tensor<Int32>
+        var classes: Tensor<Int32>
+        var indices: Tensor<Int32>
       
         // Calculate the distance between test and all data points.
         for i in 0..<self.data.shape[0] {
             distances[i] = minkowskiDistance(self.data[i], test, p: self.p)
         }
 
-        // Find the top neighbors with minimum distance.
+        // Find the top neighbor with minimum distance.
         (maxDistances, maxIndex) =
             Raw.topKV2(distances, k: Tensor<Int32>(Int32(data.shape[0])), sorted: true)
         maxDistances = Raw.reverse(maxDistances, dims: Tensor<Bool>([true]))
         maxDistances = maxDistances
-            .slice(lowerBounds: Tensor<Int32>([0]), sizes: Tensor<Int32>([Int32(self.neighbors)]))
+            .slice(lowerBounds: Tensor<Int32>([0]),
+                sizes: Tensor<Int32>([Int32(self.neighborCount)]))
 
         maxIndex = Raw.reverse(maxIndex, dims: Tensor<Bool>([true]))
         maxIndex = maxIndex
-            .slice(lowerBounds: Tensor<Int32>([0]), sizes: Tensor<Int32>([Int32(self.neighbors)]))
+            .slice(lowerBounds: Tensor<Int32>([0]),
+                sizes: Tensor<Int32>([Int32(self.neighborCount)]))
 
-        for i in 0..<self.neighbors {
+        for i in 0..<self.neighborCount {
             maxLabel[i] = self.labels[Int(maxIndex[i].scalarized())]
         }
 
-        /// Weights the neighbors based on their weighing method.
-        let labelsAndWeightsTensor = computeWeights(distances: maxDistances, labels: maxLabel)
+        // Weights the neighbors based on their weighing method.
+        let labelsAndWeightsTensor = computeWeights(
+            distances: maxDistances, labels: Tensor<Float>(maxLabel))
 
-        var classes: Tensor<Int32>
-        var indices: Tensor<Int32>
         (classes, indices) = Raw.unique(Tensor<Int32>(maxLabel))
         
-        var kClasses = Tensor<Float>(zeros: [classes.shape[0]])
+        var kClasses = Tensor<Int32>(zeros: [classes.shape[0]])
         var kWeights = Tensor<Float>(zeros: [classes.shape[0]])
 
         // Add weights based on their neighbors class.
         for i in 0..<labelsAndWeightsTensor.shape[0] {
             for j in 0..<classes.shape[0] {
                 if labelsAndWeightsTensor[i][0] == Tensor<Float>(classes[j]) {
-                    kClasses[j] = Tensor<Float>(classes[j])
+                    kClasses[j] = Tensor<Int32>(classes[j])
                     kWeights[j] = kWeights[j] + labelsAndWeightsTensor[i][1]
                 }
             }
         }
         
-        // Return class with heighest weight.
+        // Return class with highest weight.
         let resultSet = Raw.topKV2(kWeights, k: Tensor<Int32>(1), sorted: true)
         let classIndex = Int(resultSet.indices[0].scalarized())
         return kClasses[classIndex]
     }
 
-    /// Returns classified test tensor.
+    /// Returns classification.
     ///
-    /// - Parameter data: data tensor of shape [number of samples, number of features].
-    /// - Returns: classified class tensor.  
-    public func prediction(for data: Tensor<Float>) -> Tensor<Float> {
+    /// - Parameter data: Prediction data with shape `[sample count, feature count]`.
+    /// - Returns: classification for test data.  
+    public func prediction(for data: Tensor<Float>) -> Tensor<Int32> {
+        precondition(data.shape[0] > 0, "Data must be non-empty.")
 
-        precondition(data.shape[0] > 0, "data must be non-empty.")
-
-        var predictions = Tensor<Float>(zeros: [data.shape[0]])
+        var predictions = Tensor<Int32>(zeros: [data.shape[0]])
         for i in 0..<data.shape[0] {
             predictions[i] = predictSingleSample(data[i])
         }
@@ -166,15 +169,14 @@ public class KNeighborsClassifier {
     /// Returns Predict class labels for input samples.
     ///
     /// - Parameters
-    ///   - data: Sample tensor of shape [number of samples, number of features].
-    ///   - labels: Target label tensor of shape [number of samples].
+    ///   - data: Sample data with shape `[sample count, feature count]`.
+    ///   - labels: Target label with shape `[sample count]`.
     /// - Returns: Returns the mean accuracy on the given test data and labels.
-    public func score(data: Tensor<Float>, labels: Tensor<Float>) -> Float {
-        
+    public func score(data: Tensor<Float>, labels: Tensor<Int32>) -> Float {
         precondition(data.shape[0] == labels.shape[0],
-            "data and labels must have same number of samples.")
-        precondition(data.shape[0] > 0, "data must be non-empty.")
-        precondition(labels.shape[0] > 0, "labels must be non-empty.")
+            "Data and labels must have same number of samples.")
+        precondition(data.shape[0] > 0, "Data must be non-empty.")
+        precondition(labels.shape[0] > 0, "Labels must be non-empty.")
 
         let predictedLabels = self.prediction(for: data)
         var count: Int = 0
--- Sources/swiftML/KNeighborsRegressor.swift
+++ Sources/swiftML/KNeighborsRegressor.swift
@@ -5,71 +5,72 @@ import TensorFlow
 /// The target is predicted by local interpolation of the targets associated of the nearest
 /// neighbors in the training set.
 public class KNeighborsRegressor {
-
-    var neighbors: Int
+    /// The order of the norm of the difference: `||a - b||_p`.
     var p: Int
+    /// Weight function used in prediction.
     var weights: String
+    /// Number of neighbors.
+    var neighborCount: Int
+    /// The training data.
     var data: Tensor<Float>
+    /// The target value correspoing to training data.
     var labels: Tensor<Float>
   
     /// Create a K neighbors regressor model.
     ///
     /// - Parameters
-    ///   - neighbors: Number of neighbors to use, default to `5`.
+    ///   - neighborCount: Number of neighbors to use, default to `5`.
     ///   - weights: Weight function used in prediction. Possible values `uniform` - uniform
-    ///     weighted, `distance` - weight point by inverse of thier distance. Default set to
+    ///     weighted, `distance` - weight point by inverse of their distance. Default set to
     ///     `distance`.
     ///   - p: The order of the norm of the difference: `||a - b||_p`, default set to `2`.
     public init(
-        neighbors: Int = 5,
+        neighborCount: Int = 5,
         weights: String = "distance",
         p: Int = 2
     ) {
-        precondition(neighbors > 1, "Neighbors must be greater than one.")
+        precondition(neighborCount > 1, "Neighbor count must be greater than one.")
         precondition(weights == "uniform" || weights == "distance",
-            "weights must be either 'uniform' or 'distance'.")
-        precondition(p > 1, "p must be greater than one.")
+            "Weights must be either 'uniform' or 'distance'.")
+        precondition(p > 0, "p must be positive.")
 
-        self.neighbors = neighbors
+        self.neighborCount = neighborCount
         self.weights = weights
         self.p = p
         self.data = Tensor<Float>([0.0])
         self.labels = Tensor<Float>([0.0])
     }
   
-    /// fit Kneighbors regressor model.
+    /// Fit a K-neighbors regressor model.
     ///
     /// - Parameters
-    ///   - data: Training data tensor of shape [number of samples, number of features].
-    ///   - labels: Target value tensor of shape [number of samples].
+    ///   - data: Training data with shape `[sample count, feature count]`.
+    ///   - labels: Target value with shape `[sample count]`.
     public func fit(data: Tensor<Float>, labels: Tensor<Float>) {
-
         precondition(data.shape[0] == labels.shape[0],
-            "data and labels must have same number of samples.")
-        precondition(data.shape[0] > 0, "data must be non-empty.")
-        precondition(data.shape[1] >= 1, "data must have atleast single feature.")
-        precondition(labels.shape[0] > 0, "labels must be non-empty.")
+            "Data and labels must have same number of samples.")
+        precondition(data.shape[0] > 0, "Data must be non-empty.")
+        precondition(data.shape[1] >= 1, "Data must have atleast single feature.")
+        precondition(labels.shape[0] > 0, "Labels must be non-empty.")
 
         self.data = data
         self.labels = labels
     }
-
   
-    /// Return the average value of target.
+    /// Returns the average value of target.
     ///
     /// - Parameters
-    ///   - distances: Tensor contains the distance between test tensor and top neighbors.
-    ///   - labels: Tensor contains the average value of target.
+    ///   - distances: Contains the distance between test data and top neighbors.
+    ///   - labels: Contains the value of target.
     internal func computeWeights(
         distances: Tensor<Float>,
         labels: Tensor<Float>
     ) -> Tensor<Float> {
-      
         var weightsTensor = Tensor<Float>(zeros:[distances.shape[0]])
         var result = Tensor<Float>(0.0)
         
-        // In uniform weighing method each class have same weight.
-        // In distance weighing method weight vary based on the inverse of distance.
+        // In `uniform` weighing method each class have same weight.
+        // In `distance` weighing method weight vary based on the inverse of distance.
         if self.weights == "uniform" {
             return labels.mean()
         } else if self.weights == "distance" {
@@ -88,14 +89,13 @@ public class KNeighborsRegressor {
         return Tensor<Float>(0.0)
     } 
 
-    /// Returns the individual tensor predicted value.
+    /// Returns the individual predicted value.
     ///
-    /// - Parameter data: Input tensor to be regressed.
+    /// - Parameter data: Input data to be regressed.
     /// - Returns: Predicted target value.
     internal func predictSingleSample(_ test: Tensor<Float>) -> Tensor<Float> {
-
         var distances = Tensor<Float>(zeros: [self.data.shape[0]])
-        var maxLabel = Tensor<Float>(zeros: [self.neighbors])
+        var maxLabel = Tensor<Float>(zeros: [self.neighborCount])
         var maxDistances: Tensor<Float>
         var maxIndex: Tensor<Int32>
         
@@ -106,31 +106,32 @@ public class KNeighborsRegressor {
         
         // Find the top neighbors with minimum distance.
         (maxDistances, maxIndex) =
-            Raw.topKV2(distances, k: Tensor<Int32>(Int32(data.shape[0])) , sorted: true)
+            Raw.topKV2(distances, k: Tensor<Int32>(Int32(data.shape[0])), sorted: true)
         maxDistances = Raw.reverse(maxDistances, dims: Tensor<Bool>([true]))
         maxDistances = maxDistances
-            .slice(lowerBounds: Tensor<Int32>([0]), sizes: Tensor<Int32>([Int32(self.neighbors)]))
+            .slice(lowerBounds: Tensor<Int32>([0]),
+                sizes: Tensor<Int32>([Int32(self.neighborCount)]))
 
         maxIndex = Raw.reverse(maxIndex, dims: Tensor<Bool>([true]))
         maxIndex = maxIndex
-            .slice(lowerBounds: Tensor<Int32>([0]), sizes: Tensor<Int32>([Int32(self.neighbors)]))
+            .slice(lowerBounds: Tensor<Int32>([0]),
+                sizes: Tensor<Int32>([Int32(self.neighborCount)]))
 
-        for i in 0..<self.neighbors {
+        for i in 0..<self.neighborCount {
             maxLabel[i] = self.labels[Int(maxIndex[i].scalarized())]
         }
 
-        /// Average weight based on neighbors weights.
+        // Average weight based on neighbors weights.
         let avgWeight = computeWeights(distances: maxDistances, labels: maxLabel)
         return avgWeight
     }
   
-    /// Returns predicted value of test tensor.
+    /// Returns predicted values.
     ///
-    /// - Parameter data: Test tensor of shape [number of samples, number of features].
-    /// - Returns: Predicted value tensor. 
+    /// - Parameter data: Test data with shape `[sample count, feature count]`.
+    /// - Returns: Predicted value tensor.
     public func prediction(for data: Tensor<Float>) -> Tensor<Float> {
-
-        precondition(data.shape[0] > 0, "data must be non-empty.")
+        precondition(data.shape[0] > 0, "Data must be non-empty.")
 
         var predictions = Tensor<Float>(zeros: [data.shape[0]])
         for i in 0..<data.shape[0] {
@@ -139,18 +140,17 @@ public class KNeighborsRegressor {
         return predictions
     }
   
-    /// Returns the coefficient of determination R^2 of the prediction.
+    /// Returns the coefficient of determination (`R^2`) of the prediction.
     ///
     /// - Parameters
-    ///   - data: Sample tensor of shape [number of samples, number of features].
-    ///   - labels: Target value tensor of shape [number of samples].
-    /// - Returns: The coefficient of determination R^2 of the prediction.
+    ///   - data: Sample data with shape `[sample count, feature count]`.
+    ///   - labels: Target values with shape `[sample count]`.
+    /// - Returns: The coefficient of determination (`R^2`) of the prediction.
     public func score(data: Tensor<Float>, labels: Tensor<Float>) -> Float {
-
         precondition(data.shape[0] == labels.shape[0],
-            "data and labels must have same number of samples.")
-        precondition(data.shape[0] > 0, "data must be non-empty.")
-        precondition(labels.shape[0] > 0, "labels must be non-empty.")
+            "Data and labels must have same number of samples.")
+        precondition(data.shape[0] > 0, "Data must be non-empty.")
+        precondition(labels.shape[0] > 0, "Labels must be non-empty.")
 
         let predictedLabels = self.prediction(for: data)
         let u = pow((labels - predictedLabels),2).sum() 
